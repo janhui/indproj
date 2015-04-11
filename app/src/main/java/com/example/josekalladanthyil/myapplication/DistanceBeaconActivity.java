a78360cb83810bdb2ff27c7dc3c932bb3506fc04
Jose Kalladanthyil <josekalladanthyil@hotmail.com>
2015-04-11 16:40:14 +0100

    initial test

---
+++ app/src/main/java/com/example/josekalladanthyil/myapplication/DistanceBeaconActivity.java
@@ -0,0 +1,7 @@
+package com.example.josekalladanthyil.myapplication;
+
+/**
+ * Created by josekalladanthyil on 23/02/15.
+ */
+public class DistanceBeaconActivity {
+}
---
+++ app/src/main/java/com/example/josekalladanthyil/myapplication/SensorMovementActivity.java
@@ -0,0 +1,16 @@
+package com.example.josekalladanthyil.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by josekalladanthyil on 23/02/15.
+ */
+public class SensorMovementActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+    }
+
+}
